97efe8ed293a3f306ab8cca2409eed0b3b055d3b
vcc3 <vcc3@njit.edu>
2016-12-19 18:54:14 -0500

    added the text for the sql query info

--- memberpage.php
+++ memberpage.php
@@ -22,12 +22,38 @@ require('layout/header.php');
                                     <li><a href ='logout.php'>LogOut</a></li
                                   </ul>
                                 </div>
-                                <div id ="ccontent">
-                              	  <h2>Profile page - Welcome <?php echo $_SESSION['username']; ?></h2>		
-                                  <p> testing testing testing</p>
-                                  
-                                </div>
-                        </div>
+                                <div id ="ccontent">                                
+                               <h2> Profile Page - Welcome <?php echo $_SESSION['username']; ?></h2>
+                                  <?php 
+                                   $con=mysqli_connect("sql1.njit.edu","vcc3","4aYwK2YO","vcc3");
+                                // Check connection
+                                if (mysqli_connect_errno())
+                                {
+                                echo "Failed to connect to MySQL: " . mysqli_connect_error();
+                                }
+                                
+                                $result = mysqli_query($con,"SELECT * FROM members WHERE username ='vcc3'");
+                                
+                               
+                                                               
+                                while($row = mysqli_fetch_array($result))
+                                {
+                                echo "<img width='100px' height='100px' src=".$row['profilepic'].">";                               						                	
+                                echo "<p> Users memberID :" . $row['memberID'] . "</p>";
+                                echo "<p>Username is :" . $row['username'] . "</p>";
+                                echo "<p>Users email is :" . $row['email']    . "</p>";
+								                echo "<p>Bio:" . $row['info']     . "</p>";
+                              
+                                }
+                            
+                                
+                                mysqli_close($con);
+                                   
+                                   
+                                   ?>
+   
+                             </div>
+                          </div>
                        </div>
 <?php 
 //include header template
