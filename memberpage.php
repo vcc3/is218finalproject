9f4c1548b18624719bbab020daefce55d4294ff2
vcc3 <vcc3@njit.edu>
2016-12-20 16:05:48 -0500

    found way on sqlserver to set a default img location

--- memberpage.php
+++ memberpage.php
@@ -23,7 +23,7 @@ require('layout/header.php');
                                   </ul>
                                 </div>
                                 <div id ="ccontent">                                
-                               <h2>Welcome to the Editting Profile Page - Welcome <?php echo $_SESSION['username']; ?></h2>
+                               <h2>Profile Page - Welcome <?php echo $_SESSION['username']; ?></h2>
                                
                                   <?php 
                                    $con=mysqli_connect("sql1.njit.edu","vcc3","4aYwK2YO","vcc3");
@@ -33,32 +33,24 @@ require('layout/header.php');
                                 echo "Failed to connect to MySQL: " . mysqli_connect_error();
                                 }
                                 $user = $_SESSION['username'];
+                                //look at th euser logged in and show
                                 $result = mysqli_query($con,"SELECT * FROM members WHERE username ='$user'");
                                 
                                
                                                                
                                 while($row = mysqli_fetch_array($result))
-                                {
-                                //strategy to see if pic is empty then set to default else send me the img link
-                                $pic =$_SESSION['profilepic'];
                                 
-                                if(!empty($pic))
-                                  { 
-                                  echo "<img width='100px' height='100px' src='img/pro.jpg'>";                               						                	
-                                  echo "<p>Users memberID :"  . $row['memberID'] . "</p>";
-                                  echo "<p>Username is :"     . $row['username'] . "</p>";
-                                  echo "<p>Users email is :"  . $row['email']    . "</p>";
-								                  echo "<p>Bio:"              . $row['info']     . "</p>";                            
-                                  }
-                                  else 
+                               
                                   {
-                                  echo "<img width='100px' height='100px' src='img/".$row['profilepic']."'>";                               						                	
+                                  echo "<img width='100px' height='100px' src='img/".$row['profilepic']."'>";                              						              
                                   echo "<p>Users memberID :"  . $row['memberID'] . "</p>";
                                   echo "<p>Username is :"     . $row['username'] . "</p>";
                                   echo "<p>Users email is :"  . $row['email']    . "</p>";
+                                  echo "<p>Full Name:"              . $row['fullname']     . "</p>";
+                                  echo "<p>Age:"              . $row['age']     . "</p>";  
 								                  echo "<p>Bio:"              . $row['info']     . "</p>";
                                   }
-                                }
+                                
                             
                                 
                                 mysqli_close($con);
