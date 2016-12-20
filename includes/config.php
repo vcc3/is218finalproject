4b1f2505a7d30805de5de8c7887e9f82ac3de3dc
vcc3 <vcc3@njit.edu>
2016-12-20 14:21:11 -0500

    turned the config into a singleton connection woohoo

--- includes/config.php
+++ includes/config.php
@@ -1,26 +1,41 @@
 <?php
-ob_start();
-session_start();
 
-//database credentials
-define('DBHOST','sql1.njit.edu');
-define('DBUSER','vcc3');
-define('DBPASS','4aYwK2YO');
-define('DBNAME','vcc3');
-//application address
-define('DIR','https://web.njit.edu/~vcc3/is218pro1/final1/');
-define('SITEEMAIL','vcc3@njit.edu');
-try {
-	//create PDO connection
-	$db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-	//show error
+  class dbConn{
+    protected static $db;
+    private function __construct(){
+    ob_start();
+    session_start();
+    //application address
+    define('DIR','https://web.njit.edu/~vcc3/is218pro1/final1/');
+    define('SITEEMAIL','vcc3@njit.edu');
+    try {
+	  //create PDO connection
+      self::$db= new PDO('mysql:host=sql1.njit.edu;dbname=vcc3', 'vcc3', '4aYwK2YO' );	
+  	  self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } 
+    catch(PDOException $e) {
+	  //show error
     echo '<p class="bg-danger">'.$e->getMessage().'</p>';
     exit;
-}
+    }
+    }
+    public static function getConnection() {
+      
+      //Guarantees single instance, if no connection object exists then create one.
+      if (!self::$db) {
+      //new connection object.
+      new dbConn();
+      }
+       
+       //return connection.
+       return self::$db;
+       }
+       }
+
 //include the user class, pass in the database connection
 include('classes/user.php');
 include('classes/phpmailer/mail.php');
-$user = new User($db);
+$dbs = dbConn::getConnection();
+$user = new User($dbs);
 ?>
+
