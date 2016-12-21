96411aabc272d8b2d97bb854401e5f530b333d10
vcc3 <vcc3@njit.edu>
2016-12-21 11:32:18 -0500

    homepage for logged in members

---
+++ lihomepage.php
@@ -0,0 +1,49 @@
+<?php require('includes/config.php'); 
+
+
+//define page title
+$title = 'Homepage Page';
+
+//include header template
+require('layout/header.php'); 
+?>
+
+
+                       <div id ="wrapper">
+                          <div id ="header">
+                              <div id="logo">       
+                              </div>  
+                          	  	<div id="menu">
+                                  <ul>
+                                    <li><a href ='lihomepage.php'>Home</a>|</li>                                   
+                                    <li><a href ='#'>About</a>|</li>
+                                    <li><a href ='#'>News</a>|</li>
+                                    <li><a href ='memberpage.php'>Profile</a></li>
+                                    <li><a href ='logout.php'>LogOut</a></li
+                                  </ul>
+                                </div>
+                          <div id ="ccontent">                                
+                               <h2>Welcome to ******.com</h2>
+                             <p>
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas a dolor eleifend vestibulum. Morbi bibendum, sapien varius porttitor viverra, lorem nisl malesuada dui, sed blandit dui mauris eget nibh. Cras ac auctor odio. Cras tempor maximus enim vel finibus. Etiam egestas justo vel orci convallis, ac tincidunt nunc sollicitudin. Ut nec magna sed risus condimentum tempus. Etiam dapibus congue tristique. In hac habitasse platea dictumst. Aliquam ornare condimentum varius. Quisque dapibus eros magna, non semper ligula efficitur vel. Cras facilisis, eros at aliquet tincidunt, velit dolor hendrerit nibh, sollicitudin eleifend enim lacus a nunc. Vestibulum auctor, risus non iaculis euismod, nibh lectus ullamcorper nibh, eu tristique ante tellus in ipsum. Nunc et auctor eros. Aenean laoreet porttitor velit.
+
+Duis viverra malesuada ultrices. Ut massa lectus, pulvinar a dictum a, pretium id tellus. Donec finibus accumsan molestie. Morbi suscipit orci in ante sollicitudin, et condimentum magna accumsan. Nunc vitae blandit odio. Vivamus convallis semper purus, a fringilla ipsum. In odio nisl, viverra a velit ac, vestibulum eleifend nunc. Sed scelerisque, mauris non pellentesque consectetur, lacus nulla convallis mi, volutpat lobortis tellus ipsum porta leo. Sed hendrerit tristique ante, at eleifend quam dapibus at. Cras pulvinar eros sed ipsum laoreet elementum. Donec cursus nisl a sem venenatis congue eget vel velit. Pellentesque imperdiet lacus sed pharetra accumsan. Nulla porta ornare fermentum.
+
+Vivamus odio ipsum, posuere vitae molestie non, porta non ligula. Donec at pretium erat. Nulla blandit lobortis arcu quis tincidunt. Suspendisse lobortis odio id magna cursus dictum. Nullam eu ultricies arcu. Etiam commodo dictum ultrices. Ut nec arcu ullamcorper, dictum velit eu, euismod dolor.
+
+Morbi malesuada tincidunt lectus et efficitur. Cras nisi turpis, bibendum sed egestas sit amet, accumsan id tortor. Aenean lacinia blandit vulputate. In hac habitasse platea dictumst. Suspendisse sed odio sagittis, tempor augue eget, vestibulum nunc. Ut erat risus, tempor ut gravida et, blandit sed lacus. Phasellus quis tincidunt odio.
+
+Nulla eu ullamcorper lorem. Sed facilisis tellus vel enim fermentum, venenatis placerat erat placerat. Phasellus quis metus a lorem interdum convallis. Aenean sit amet neque vel quam dignissim eleifend. Aliquam erat volutpat. Vestibulum sit amet dui in eros pulvinar porta. Curabitur lacinia pretium nisi, ut vestibulum sem vulputate ac. Nullam ac magna in nisl congue rhoncus eget pharetra quam. Praesent facilisis mauris vel erat mattis maximus. Pellentesque risus nisi, mattis a dui ut, malesuada interdum ante. Quisque mattis tellus elit, vitae eleifend dolor blandit eget. Praesent posuere, neque eu tristique dapibus, sapien tortor pharetra risus, non condimentum lectus nibh nec mi. Vestibulum ornare sed nisl ut pellentesque. Donec id placerat urna, ut finibus massa. Nullam lacinia pellentesque diam, non luctus lectus aliquet vel. Donec finibus ac arcu sagittis tristique. </p>
+                
+                              
+                              
+                                  
+                            </div>
+                          </div>
+                       </div>
+<?php 
+//include header template
+require('layout/footer.php'); 
+?>
+y
